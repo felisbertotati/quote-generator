b05bd780998dafb26698c7943d2117b8f8ae61f2
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-04-27 15:50:37 +0100

    - added loader

--- Assets/script.js
+++ Assets/script.js
@@ -3,14 +3,28 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 // Get quotes from API
 let apiQuotes = [];
+
+//Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // show new Quote
 
 function newQuote() {
   //Pick a random quote from apiQuotes array
-
+  loading();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   //check if Author field is blank and replace it with 'unknown'
   if (!quote.author) {
@@ -25,11 +39,13 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
-
+  // set quote, hide loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
